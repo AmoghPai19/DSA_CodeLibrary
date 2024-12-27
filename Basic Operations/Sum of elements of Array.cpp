d85d72bbe8b0fc7c430c855b9b817b860b716ead
AmoghPai19 <amogh1924@gmail.com>
2024-12-27 22:44:56 +0530

    Update Sum of elements of Array.cpp

--- Basic Operations/Sum of elements of Array.cpp
+++ Basic Operations/Sum of elements of Array.cpp
@@ -4,20 +4,19 @@ using namespace std;
 int main() {
     int n, sum = 0;
 
-    // Ask the user to input the number of elements in the array
+    
     cout << "Enter the number of elements: ";
     cin >> n;
 
-    // Declare an array of size 'n'
+   
     int arr[n];
 
-    // Ask the user to input the array elements
+
     cout << "Enter the elements of the array:" << endl;
     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
 
-    // Calculate the sum of the array elements
     for (int i = 0; i < n; ++i) {
         sum += arr[i];
     }
